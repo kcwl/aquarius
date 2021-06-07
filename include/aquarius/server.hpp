1d983be5dd441ab40053fecfaa235b100918a7e5
kcwl <767899122@qq.com>
2021-06-07 23:17:17 +0800

    完善server

--- include/aquarius/server.hpp
+++ include/aquarius/server.hpp
@@ -6,88 +6,87 @@
 
 namespace aquarius
 {
-	namespace aqnet
+	class server
 	{
-		class server
+	public:
+		explicit server(const std::string& port, int io_service_pool_size)
+			: io_service_pool_(io_service_pool_size)
+			, signals_(io_service_pool_.get_io_service())
+			, acceptor_(io_service_pool_.get_io_service(), boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), static_cast<unsigned short>(std::atoi(port.data()))))
+#ifdef _SSL_SERVER
+			, ssl_context_(boost::asio::ssl::context::sslv23)
 		{
-		public:
-			explicit server(const std::string& port, int io_service_pool_size)
-				: io_service_pool_(io_service_pool_size)
-				, signals_(io_service_pool_.get_io_service())
-				, acceptor_(io_service_pool_.get_io_service(), boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), std::atoi(port.c_str())))
-#ifdef _SSL_SERVER
-				, _context(boost::asio::ssl::context::sslv23)
-				{
-					init_ssl_param(std::bind(&aq_server::get_passwd, this), "", "", "");
+			init_ssl_param(std::bind(&server::get_passwd, this), "", "", "");
 #else
-				{
+			{
 #endif
-					signals_.add(SIGINT);
-					signals_.add(SIGTERM);
+				signals_.add(SIGINT);
+				signals_.add(SIGTERM);
 #ifdef SIGQUIT
-					signals_.add(SIGQUIT);
+				signals_.add(SIGQUIT);
 #endif
-					signals_.async_wait(std::bind(&server::handle_stop, this));
+				signals_.async_wait(std::bind(&server::handle_stop, this));
 
-					start_accept();
-				}
-
-			virtual~server() = default;
-
-		public:
-			void run()
-			{
-				io_service_pool_.run();
+				start_accept();
 			}
 
-		private:
-			void start_accept()
-			{
-				auto new_connect_ptr = std::make_shared<connect>(io_service_pool_.get_io_service());
+		virtual~server() = default;
 
-				acceptor_.async_accept(new_connect_ptr->socket(),
-					[this, new_connect_ptr](const boost::system::error_code& error)
-					{
-						if(!error)
-						{
-							new_connect_ptr->start();
-
-							start_accept();
-						}
-					}
-				);
-			}
-
-			void handle_stop()
-			{
-				io_service_pool_.stop();
-			}
-
-#ifdef _SSL_SERVER
-			template<class Func>
-			void init_ssl_param(Func && f, std::string certificate_chain_file, std::string private_key_file, std::string tmp_dh_file)
-			{
-				_context.set_password_callback(std::forward<Func>(f));
-
-				_context.use_certificate_chain_file(certificate_chain_file);
-				_context.use_private_key_file(private_key_file, boost::asio::ssl::context::pem);
-				_context.use_tmp_dh_file(tmp_dh_file);
+	public:
+		void run()
+		{
+			io_service_pool_.run();
 		}
-#endif
 
-		private:
-			io_service_pool io_service_pool_;
+	private:
+		void start_accept()
+		{
+			auto new_connect_ptr = std::make_shared<connect>(io_service_pool_.get_io_service());
 
-			boost::asio::signal_set signals_;
+			acceptor_.async_accept(new_connect_ptr->socket(),
+								   [this, new_connect_ptr](const boost::system::error_code& error)
+								   {
+									   if(!error)
+									   {
+										   new_connect_ptr->start();
 
-			tcp::acceptor acceptor_;
+										   start_accept();
+									   }
+								   }
+			);
+		}
 
-			int size_;
+		void handle_stop()
+		{
+			io_service_pool_.stop();
+		}
 
 #ifdef _SSL_SERVER
-			boost::asio::ssl::context _context;
+		template<class Func>
+		void init_ssl_param(Func && f, std::string certificate_chain_file, std::string private_key_file, std::string tmp_dh_file)
+		{
+			ssl_context_.set_password_callback(std::forward<Func>(f));
+
+			ssl_context_.use_certificate_chain_file(certificate_chain_file);
+			ssl_context_.use_private_key_file(private_key_file, boost::asio::ssl::context::pem);
+			ssl_context_.use_tmp_dh_file(tmp_dh_file);
+		}
+
+		void get_passwd() {}
 #endif
-			std::shared_ptr<connect> new_connection_ptr_;
+
+	private:
+		io_service_pool io_service_pool_;
+
+		boost::asio::signal_set signals_;
+
+		tcp::acceptor acceptor_;
+
+		int size_;
+
+#ifdef _SSL_SERVER
+		boost::asio::ssl::context ssl_context_;
+#endif
+		std::shared_ptr<connect> new_connection_ptr_;
 	};
-}
 }
