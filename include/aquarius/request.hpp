8f307f27027e49ed6fc09aa844343bd121ffbbc8
kcwl <767899122@qq.com>
2021-06-07 23:19:33 +0800

    重构message体系

--- include/aquarius/request.hpp
+++ include/aquarius/request.hpp
@@ -1,49 +1,67 @@
 ﻿#pragma once
-#include "detail\message.hpp"
-#include "detail\header.hpp"
+#include "message.hpp"
+#include "header.hpp"
+#include "detail/router.hpp"
 
 namespace aquarius
 {
-	namespace aqnet
+	template<class Body, std::size_t Number>
+	class request
+		: public message<request_header, Body, Number>
+		, public std::enable_shared_from_this<request<Body, Number>>
 	{
-		template<class Body, std::size_t Number>
-		class tcp_request : public detail::request<tcp_request_header, Body, Number>
+	public:
+		request() = default;
+		virtual ~request() = default;
+
+	public:
+		virtual std::size_t parse_bytes(detail::streambuf& ios) override
 		{
-		public:
-			virtual std::size_t parse_bytes(iostream&& stream, std::size_t length) override
-			{
-				this->header()->parse_bytes(std::forward<iostream>(stream), length);
+			// 处理header
+			ios >> this->header()->proto_id_ >> this->header()->part_id_ >> this->header()->reserve_ >> this->header()->src_id_ >> this->header()->session_id_;
 
-				if(this->header()->length_ + sizeof(tcp_request_header) != length)
-					return -1;
+			// 处理body
+			this->body()->parse_bytes(ios);
 
-				if(this->header()->protocol_ == 0)
-					return -1;
+			//触发ctx
+			auto ctx_ptr = detail::router::instance().route_func("ctx_" + std::to_string(this->header()->proto_id_));
 
-				auto size = sizeof(uint32_t);
-				length -= size;
+			if(ctx_ptr == nullptr)
+				return 0;
 
-				if(this->header()->sub_proto_ == 0)
-					return -1;
+			return this->accept(this->shared_from_this(), ctx_ptr, this->get_conn_ptr());
+		}
 
-				length -= size * 2;
-
-				if(length == 0)
-					return 0;
-
-				return this->body()->parse_bytes(std::forward<iostream>(stream), length);
-			}
-
-			virtual void* to_bytes()
-			{
-				return nullptr;
-			}
-		};
-
-		template<class Body, std::size_t Number>
-		class http_request : public detail::request<http_request_header, Body, Number>
+		virtual std::size_t to_bytes(detail::streambuf& ios) override
 		{
+			// 处理header
+			ios << this->header()->proto_id_ << this->header()->part_id_ << this->header()->reserve_ << this->header()->src_id_ <<  this->header()->session_id_;
 
-		};
-	}
+			// 处理body
+			this->body()->to_bytes(ios);
+
+			return 1;
+		}
+
+	public:
+		uint32_t get_src_id() 
+		{
+			return this->header()->src_id_;
+		}
+
+		void set_src_id(uint32_t src_id)
+		{
+			this->header()->src_id_ = src_id;
+		}
+
+		uint32_t get_session_id()
+		{
+			return this->header()->session_id_;
+		}
+
+		void set_session_id(uint32_t session_id)
+		{
+			this->header()->session_id_ = session_id;
+		}
+	};
 }
