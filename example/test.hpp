a619ffb5edfd7a1b72c0d63fa8dc936ad56d294b
kcwl <liuweiliang1@kingsoft.com>
2021-07-19 14:09:00 +0800

    optimize for process message

--- example/test.hpp
+++ example/test.hpp
@@ -1,48 +1,19 @@
 #pragma once
 #include <string>
-#include <aquarius/message_body.hpp>
 #include <aquarius/request.hpp>
 #include <aquarius/response.hpp>
 #include <aquarius/context.hpp>
 #include <aquarius/detail/router.hpp>
 
 
-class login_request_body : public aquarius::message_body
+struct login_request_body
 {
-public:
-	virtual std::size_t parse_bytes(aquarius::detail::streambuf& stream) override
-	{
-		stream << user_name_ << passwd_;
-
-		return 12;
-	}
-
-	virtual std::size_t to_bytes(aquarius::detail::streambuf& stream) override
-	{
-		stream >> user_name_ >> passwd_;
-		return 12;
-	}
-
-public:
 	std::string user_name_;
 	std::string passwd_;
 };
 
-class login_response_body : public aquarius::message_body
+class login_response_body
 {
-public:
-	virtual std::size_t parse_bytes(aquarius::detail::streambuf& stream)
-	{
-		stream >> result_;
-		return 4;
-	}
-
-	virtual std::size_t to_bytes(aquarius::detail::streambuf& stream)
-	{
-		stream << result_;
-		return 4;
-	}
-
 public:
 	int result_;
 };
@@ -55,7 +26,7 @@ class login_handler : public aquarius::handler<login_request, login_response>
 public:
 	virtual bool handle() override
 	{
-		resp_.body()->result_ = 1;
+		resp_.body().result_ = 1;
 
 		return true;
 	}
