789ac31e04781360a159e80df7ed2ee9337fdde1
kcwl <liuweiliang1@kingsoft.com>
2021-08-23 09:32:51 +0800

    append async timer

--- include/aquarius/detail/deadline_timer.hpp
+++ include/aquarius/detail/deadline_timer.hpp
@@ -48,9 +48,9 @@ public:
 
 		time_t des_time = std::mktime(t);
 
-		des_time < now ? des_time += 24 * 60 * 60 : 0;
+		des_time < now.count() ? des_time += 24 * 60 * 60 : 0;
 
-		return async_wait<decltype(f()),Func>(des_time,std::move(f))
+		return async_wait<decltype(f()), Func>(des_time, std::move(f));
 	}
 
 private:
@@ -92,7 +92,7 @@ private:
 						  {
 							  if(!is_expirse_)
 							  {
-								  is_use_.stroe(false);
+								  is_use_.store(false);
 								  break;
 							  }
 
