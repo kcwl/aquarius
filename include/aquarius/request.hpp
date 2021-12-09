4f7402d21d153ba6e63b44893d727593ebc760ee
kcwl <liuweiliang1@kingsoft.com>
2021-12-09 17:28:11 +0800

    修改消息操作触发流程

--- include/aquarius/request.hpp
+++ include/aquarius/request.hpp
@@ -5,7 +5,8 @@ namespace aquarius
 {
 	template<class _Body, std::size_t Number>
 	class request
-		: public message<request_header, _Body, Number>
+		: protected message<request_header, _Body, Number>
+		, protected visiable<context, send_response_t>
 		, public std::enable_shared_from_this<request<_Body, Number>>
 	{
 	public:
@@ -25,6 +26,11 @@ namespace aquarius
 			ios << this->header()->proto_id_ << this->header()->part_id_ << this->header()->reserve_ << this->header()->src_id_ <<  this->header()->session_id_;
 
 			this->body().to_bytes(ios);
+		}
+
+		virtual int accept(std::shared_ptr<context> ctx_ptr, send_response_t&& f)
+		{
+			return accept_impl(this->shared_from_this(), ctx_ptr, std::forward<send_response_t>(f));
 		}
 
 	public:
--- include/aquarius/visitor.hpp
+++ include/aquarius/visitor.hpp
@@ -2,11 +2,35 @@
 
 namespace aquarius
 {
+	class basic_message;
+
 	template <typename Request, typename Func, typename ReturnT = int>
 	class visitor
 	{
 	public:
 		virtual ~visitor() {}
-		virtual ReturnT visit(std::shared_ptr<Request> visited,Func&& f) = 0;
+		virtual ReturnT visit(std::shared_ptr<Request> visited, Func&& f) = 0;
+	};
+
+
+	template<typename Context, typename Func>
+	class visiable
+	{
+	public:
+		virtual int accept(std::shared_ptr<Context> ctx_ptr, Func&& f) = 0;
+
+	protected:
+		template<typename Request, typename Context, typename Func>
+		int accept_impl(std::shared_ptr<Request> msg_ptr, std::shared_ptr<Context> ctx_ptr, Func&& f)
+		{
+			using request_type = visitor<Request, Func, int>;
+
+			auto request_ptr = std::dynamic_pointer_cast<request_type>(ctx_ptr);
+
+			if (request_ptr == nullptr)
+				return 0;
+
+			return request_ptr->visit(msg_ptr, std::forward<Func>(f));
+		}
 	};
 }
