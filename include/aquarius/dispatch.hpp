de406a8420f43f2393749b0624c97f145120b045
kcwl <767899122@qq.com>
2022-04-27 07:53:41 +0800

    add

--- include/aquarius/dispatch.hpp
+++ include/aquarius/dispatch.hpp
@@ -5,44 +5,7 @@
 
 namespace aquarius
 {
-	namespace
-	{
-		template<typename T, typename Archive>
-		void serialize(T& value, Archive&& ar)
-		{
-			ar << value;
-		}
+	
 
-		template<typename T, typename Archive>
-		void deserialize(T& value, Archive&& ar)
-		{
-			ar >> value;
-		}
-	}
-
-	template<typename Request, typename Context>
-	int accept(std::shared_ptr<Context> ctx_ptr, std::shared_ptr<Request> msg_ptr)
-	{
-		if (msg_ptr == nullptr)
-			return 1;
-
-		using request_type = visitor<Request, int>;
-
-		auto request_ptr = std::dynamic_pointer_cast<request_type>(ctx_ptr);
-
-		if (request_ptr == nullptr)
-		{
-			return ctx_ptr->visit(std::make_shared<null_message>(msg_ptr));
-		}
-
-		return request_ptr->visit(msg_ptr);
-	}
-
-	template<typename Request, typename Context>
-	void dispatch(std::shared_ptr<Request> msg_ptr, std::shared_ptr<Context> ctx_ptr, msg::header_value header, ftstream& buf)
-	{
-		deserialize(*msg_ptr, buf);
-
-		accept(ctx_ptr, msg_ptr);
-	}
+	
 }
