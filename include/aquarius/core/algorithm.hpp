8296db73eb08f38ae7bfdde16fc24a1ce5e92d92
kcwl <767899122@qq.com>
2022-10-09 11:17:24 +0800

    stream fix

--- include/aquarius/core/algorithm.hpp
+++ include/aquarius/core/algorithm.hpp
@@ -6,16 +6,6 @@ namespace aquarius
 {
 	namespace core
 	{
-		template<typename _Ty, typename _Func, std::size_t... I>
-		constexpr void for_each_impl(_Ty&& val, _Func func, std::index_sequence<I...>)
-		{
-			return (func(reflect::get<I>(std::forward<_Ty>(val))), ...);
-		}
-
-		template<typename _Ty, typename _Func, std::size_t N = reflect::rf_size_v<_Ty>, typename Indices = std::make_index_sequence<N>>
-		constexpr void for_each(_Ty&& val, _Func func)
-		{
-			return for_each_impl(std::forward<_Ty>(val), func, Indices{});
-		}
+		
 	}
 }
