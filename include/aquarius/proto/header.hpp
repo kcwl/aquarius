b77cfc7cc0f28d3ccc2bcd58b8e2c9bfaa428801
kcwl <767899122@qq.com>
2022-10-09 13:32:55 +0800

    移除protobuf对于header的支持，改用POD类型

---
+++ include/aquarius/proto/header.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+
+namespace aquarius
+{
+	struct tcp_header
+	{
+		uint32_t opcode_;
+		int32_t size_;
+	};
+
+	struct tcp_request_header : tcp_header
+	{
+		uint32_t uid_;
+	};
+
+	struct tcp_response_header : tcp_header
+	{
+		int32_t result_;
+	};
+}
--- include/aquarius/proto/proto_def.hpp
+++ include/aquarius/proto/proto_def.hpp
@@ -1,12 +1,12 @@
 #pragma once
+#include "header.hpp"
 #include "ping.pb.h"
-#include "header.pb.h"
 #include "../request.hpp"
 #include "../response.hpp"
 
 namespace aquarius
 {
-	using ping_request = request<packet_response_header, ping_request_body, 100>;
-	using ping_response = response<packet_response_header, ping_response_body, 101>;
+	using ping_request = request<tcp_request_header, ping_request_body, 100>;
+	using ping_response = response<tcp_response_header, ping_response_body, 101>;
 }
 
