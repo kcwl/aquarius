303a707a041b11d9d6e8d09f1cfabf54d143f47e
kcwl <767899122@qq.com>
2023-01-06 11:13:44 +0800

    format

--- include/aquarius/context.hpp
+++ include/aquarius/context.hpp
@@ -3,7 +3,7 @@
 
 namespace aquarius
 {
-#define MESSAGE_DEFINE(base_type, type) static msg_regist < base_type, type> req##type(type::Number);
+#define MESSAGE_DEFINE(base_type, type) static msg_regist<base_type, type> req##type(type::Number);
 #define CONTEXT_DEFINE(type) static ctx_regist<type> ctx##type(type::Number);
 
-}
+} // namespace aquarius
