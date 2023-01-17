0c3cb0269f36917e07b6e4bfa78d16ee4060b8b8
kcwl <767899122@qq.com>
2023-01-17 11:19:26 +0800

    隐藏msg regist

--- include/aquarius/context.hpp
+++ include/aquarius/context.hpp
@@ -4,8 +4,11 @@
 
 namespace aquarius
 {
-#define CONTEXT_DEFINE(msg, context) static aquarius::impl::ctx_regist<context> ctx##context(msg::Number);
-
 #define MESSAGE_DEFINE(msg) static aquarius::impl::msg_regist<msg> msg##msg(msg::Number);
+
+#define CONTEXT_DEFINE(msg, context) \
+	MESSAGE_DEFINE(msg) static aquarius::impl::ctx_regist<context> ctx##context(msg::Number);
+
+
 
 } // namespace aquarius
