1213b2923ab167867f3641a63ea5001de270e4a3
kcwl <767899122@qq.com>
2023-11-23 11:46:15 +0800

    add system_context

--- include/aquarius.hpp
+++ include/aquarius.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #define BOOST_BIND_GLOBAL_PLACEHOLDERS
 #include <aquarius/client.hpp>
-#include <aquarius/message/context.hpp>
+#include <aquarius/context.hpp>
 #include <aquarius/request.hpp>
 #include <aquarius/response.hpp>
 #include <aquarius/server.hpp>
