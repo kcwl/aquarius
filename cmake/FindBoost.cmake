e267e6340a4580525c5dc9472187d356e66fecee
kcwl <767899122@qq.com>
2024-03-15 23:01:47 +0800

    modify boost cmake

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -1,8 +1,18 @@
 set(BOOST_VERSION 1.75.0)
 
-find_package(Boost ${BOOST_VERSION} QUIET NO_MODULE)
+set(AQUARIUS_NEED_BOOST 
+	unit_test_framework
+	json
+	log
+	thread
+	system
+)
+
+find_package(Boost ${BOOST_VERSION} QUIET NO_MODULE COMPONENTS ${AQUARIUS_NEED_BOOST})
+
 if(Boost_FOUND)
 message("Boost ${BOOST_VERSION} found.")
+return()
 endif()
 
 unset(INCLUDE_DIR CACHE)
@@ -16,20 +26,16 @@ endif()
 
 list(APPEND Boost_INCLUDE_DIRS ${INCLUDE_DIR})
 
-unset(LIBS CACHE)
+foreach(lib ${AQUARIUS_NEED_BOOST})
+	unset(LIBS CACHE)
+	find_library(LIBS boost_${lib})
 
+	if(NOT LIBS)
+		message(STATUS "boost_${lib} not found")
+	endif()
 
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-    set(CPUX "64")
-endif()
-
-file(GLOB BOOST_LIB_PATH "libboost*.so" "/usr/lib${CPUX}")
-
-file(GLOB LIBS "${BOOST_LIB_PATH}/libboost*.so")
-
-if(LIBS)
-list(APPEND Boost_LIBRARIES ${LIBS})
-endif()
+	list(APPEND Boost_LIBRARIES ${LIBS})
+endforeach()
 
 if(Boost_INCLUDE_DIRS AND Boost_LIBRARIES)
 set(Boost_FOUND true)
