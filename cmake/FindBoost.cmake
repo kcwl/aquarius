72cb7d86056cc44289b20e9abfbd15e18e106ed7
kcwl <767899122@qq.com>
2024-03-17 16:08:46 +0800

    fix for vs cmakelists

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -11,36 +11,33 @@ set(AQUARIUS_NEED_BOOST
 find_package(Boost ${BOOST_VERSION} QUIET NO_MODULE COMPONENTS ${AQUARIUS_NEED_BOOST})
 
 if(Boost_FOUND)
-message("Boost ${BOOST_VERSION} found.")
+message(STATUS "Boost ${BOOST_VERSION} found.")
 return()
 endif()
 
-unset(INCLUDE_DIR CACHE)
-find_path(INCLUDE_DIR
-	NAMES boost/version.hpp
-	HINTS ./
-)
+if(WIN32)
+	set(BOOST_ROOT $ENV{BOOST_ROOT})
+	if(NOT BOOST_ROOT)
+		message(FATAL_ERROR "BOOST_ROOT not found, maybe need to set")
+	endif()
 
-if(NOT INCLUDE_DIR)
-message(STATUS "Boost not found.")
-return()
-endif()
+	set(Boost_INCLUDE_DIRS ${BOOST_ROOT})
+	set(Boost_LIBRARIES ${BOOST_ROOT}/stage/lib/)
 
-list(APPEND Boost_INCLUDE_DIRS ${INCLUDE_DIR})
+else()
+	unset(INCLUDE_DIR CACHE)
+	find_path(INCLUDE_DIR
+		NAMES boost/version.hpp
+		HINTS ./
+	)
 
-file(READ ${Boost_INCLUDE_DIRS}/boost/version.hpp BOOST_VERSION_HPP)
+	if(NOT INCLUDE_DIR)
+	message(FATAL_ERROR "Boost not found.")
+	endif()
 
-if(BOOST_VERSION_HPP)
-string(REGEX MATCH "\"[0-9]_[0-9]+\"" BOOST_VERSION_INT ${BOOST_VERSION_HPP})
-endif()
+	list(APPEND Boost_INCLUDE_DIRS ${INCLUDE_DIR})
 
-
-string(REPLACE "\"" "" BOOST_VERSION_INT ${BOOST_VERSION_INT})
-string(REPLACE "_" "." BOOST_VERSION_INT ${BOOST_VERSION_INT})
-
-
-
-foreach(lib ${AQUARIUS_NEED_BOOST})
+	foreach(lib ${AQUARIUS_NEED_BOOST})
 	unset(LIBS CACHE)
 	find_library(LIBS boost_${lib})
 
@@ -52,12 +49,27 @@ foreach(lib ${AQUARIUS_NEED_BOOST})
 
 	string(APPEND BOOST_COMPONENTS ${lib})
 	string(APPEND BOOST_COMPONENTS " ")
-endforeach()
+	endforeach()
+endif()
+
+file(READ ${Boost_INCLUDE_DIRS}/boost/version.hpp BOOST_VERSION_HPP)
+
+if(BOOST_VERSION_HPP)
+string(REGEX MATCH "\"[0-9]_[0-9]+\"" BOOST_VERSION_INT ${BOOST_VERSION_HPP})
+endif()
+
+string(REPLACE "\"" "" BOOST_VERSION_INT ${BOOST_VERSION_INT})
+string(REPLACE "_" "." BOOST_VERSION_INT ${BOOST_VERSION_INT})
 
 if(Boost_INCLUDE_DIRS AND Boost_LIBRARIES)
 set(Boost_FOUND true)
 set(Boost_FOUND_VERSION ${BOOST_VERSION_INT})
-message(STATUS "Find Boost(${Boost_FOUND_VERSION}) Components ${BOOST_COMPONENTS}")
+
+if(BOOST_COMPONENTS)
+set(BOOST_COMPONENTS " Components ${BOOST_COMPONENTS}")
+endif()
+
+message(STATUS "Find Boost(${Boost_FOUND_VERSION}) ${BOOST_COMPONENTS}")
 endif()
 
 
--- cmake/FindOpenSSL.cmake
+++ cmake/FindOpenSSL.cmake
@@ -3,12 +3,25 @@ if(OpenSSL_FOUND)
 message("OpenSSL ${OpenSSL_FOUND_VERSION} found.")
 endif()
 
+
+if(WIN32)
+set(OPENSSL_ROOT $ENV{OPENSSL_ROOT})
+
+if(NOT OPENSSL_ROOT)
+message(FATAL_ERROR "OpenSSL not found. maybe need to set OPENSSL_ROOT.")
+endif()
+
+set(OpenSSL_INCLUDE_DIRS ${OPENSSL_ROOT}/include)
+file(GLOB OpenSSL_LIBRARIES "*.lib" ${OPENSSL_ROOT}/lib/*.lib)
+
+else()
+
 unset(INCLUDE_DIR CACHE)
 
 file (GLOB INCLUDE_DIR "openssl/*" /usr/include)
 
 if(NOT INCLUDE_DIR)
-message(STATUS FATAL_ERROR "OpenSSL Includes not found.")
+message(FATAL_ERROR "OpenSSL not found.")
 endif()
 
 list(APPEND OpenSSL_INCLUDE_DIRS ${INCLUDE_DIR})
@@ -20,12 +33,13 @@ find_library(OpenSSL_CRYPTO crypto)
 find_library(OpenSSL_SSL ssl)
 
 list(APPEND OpenSSL_LIBRARIES ${OpenSSL_CRYPTO} ${OpenSSL_SSL})
+endif()
 
 set(OPENSSL_COMMAND "openssl")
 set(COMMAND_ARGS "version")
 execute_process(COMMAND ${OPENSSL_COMMAND} ${COMMAND_ARGS} OUTPUT_VARIABLE OPENSSL_VERSION)
 
-string(STRIP ${OPENSSL_VERSION} OPENSSL_VERSION)
+string(STRIP "${OPENSSL_VERSION}" OPENSSL_VERSION)
 
 if(OpenSSL_INCLUDE_DIRS AND OpenSSL_LIBRARIES)
 set(OpenSSL_FOUND true)
