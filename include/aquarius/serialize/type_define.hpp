67622355ad6d584540a278996ef011baaae33d62
kcwl <767899122@qq.com>
2024-10-04 17:02:23 +0800

    elastic fix error for uint32 and uint64

--- include/aquarius/serialize/type_define.hpp
+++ include/aquarius/serialize/type_define.hpp
@@ -4,7 +4,7 @@
 
 using int32 = int32_t;
 using int64 = int64_t;
-using uint32_t = uint32_t;
-using uint64_t = uint64_t;
+using uint32 = uint32_t;
+using uint64 = uint64_t;
 using string = std::string;
 using bytes = std::vector<uint8_t>;
