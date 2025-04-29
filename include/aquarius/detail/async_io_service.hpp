8bcb580726dd3ead8b52506105fb5d9e3c5c2977
kcwl <767899122@qq.com>
2025-04-29 20:30:22 +0800

    merge ssl and no-ssl for async_io_service

--- include/aquarius/detail/async_io_service.hpp
+++ include/aquarius/detail/async_io_service.hpp
@@ -1,9 +1,8 @@
 #pragma once
 #include <aquarius/core/logger.hpp>
-#include <aquarius/detail/io_concepts.hpp>
-#include <aquarius/detail/async_io_base.hpp>
 #include <boost/asio/redirect_error.hpp>
 #include <boost/asio/use_awaitable.hpp>
+#include <aquarius/detail/ssl_traits.hpp>
 
 #define AQUARIUS_ENABLE_SSL
 
@@ -13,28 +12,45 @@
 
 namespace aquarius
 {
-
-	template <typename Protocol, typename Executor, typename = void>
-	class async_io_service
-		: public boost::asio::detail::execution_context_service_base<async_io_service<Protocol, Executor, void>>,
-		  public async_io_base<Protocol, Executor>
+	template <typename Protocol, typename Executor, bool IsServer = true, typename SSL = void>
+	class async_io_service : public boost::asio::detail::execution_context_service_base<
+								 async_io_service<Protocol, Executor, IsServer, SSL>>
 	{
 	public:
+		constexpr static auto handshake = static_cast<boost::asio::ssl::stream_base::handshake_type>(IsServer);
+
+		constexpr static std::size_t ssl_version = ssl_version_traits<SSL>::value;
+
 		using resolver_type = typename Protocol::resolver;
 
 		using execution_base_type =
-			boost::asio::detail::execution_context_service_base<async_io_service<Protocol, Executor>>;
-
-		using io_base = async_io_base<Protocol, Executor>;
+			boost::asio::detail::execution_context_service_base<async_io_service<Protocol, Executor, IsServer, SSL>>;
 
 		using executor_type = Executor;
 
-		using typename io_base::socket_type;
+		using ssl_context_type = std::conditional_t<std::is_same_v<SSL, void>, int,
+#ifdef AQUARIUS_ENABLE_SSL
+													boost::asio::ssl::context&
+#else
+													int
+#endif
+													>;
+		using socket_type = boost::asio::basic_stream_socket<Protocol, Executor>;
 
-		using ssl_type = void;
+		using ssl_socket_type = std::conditional_t<std::is_same_v<SSL, void>, null_ssl<socket_type, ssl_context_type>,
+#ifdef AQUARIUS_ENABLE_SSL
+												   boost::asio::ssl::stream<socket_type&>
+#else
+												   null_ssl<socket_type, ssl_context_type>
+#endif
+												   >;
 
-		struct implementation_type : io_base::implementation_type_base
-		{};
+		struct implementation_type
+		{
+			socket_type* socket;
+
+			ssl_socket_type* ssl_socket;
+		};
 
 	public:
 		explicit async_io_service(const executor_type& ex)
@@ -53,14 +69,37 @@ namespace aquarius
 			// this->base_construct(impl);
 		}
 
-		void move_copy(implementation_type& impl, socket_type socket)
+		void move_copy(implementation_type& impl, socket_type socket, ssl_context_type ssl_context)
 		{
-			this->base_move_copy(impl, std::move(socket));
+			impl.socket = new socket_type(std::move(socket));
+
+			impl.ssl_socket = new ssl_socket_type(*impl.socket, ssl_context);
 		}
 
 		void destroy(implementation_type& impl)
 		{
-			this->base_destroy(impl);
+			if (!impl.socket->is_open())
+				return;
+
+			boost::system::error_code ec{};
+
+			if (impl.socket)
+			{
+				impl.socket->shutdown(boost::asio::socket_base::shutdown_both, ec);
+
+				impl.socket->close(ec);
+
+				delete impl.socket;
+			}
+
+			if (impl.ssl_socket)
+			{
+				boost::system::error_code ec;
+
+				impl.ssl_socket->shutdown(ec);
+
+				delete impl.ssl_socket;
+			}
 		}
 
 		virtual void shutdown() override
@@ -80,160 +119,52 @@ namespace aquarius
 
 		template <typename ConstBuffer>
 		auto async_read_some(implementation_type& impl, ConstBuffer& buffer, std::size_t& bytes_transferred,
-					   boost::system::error_code& ec) -> boost::asio::awaitable<void>
+							 boost::system::error_code& ec) -> boost::asio::awaitable<void>
 		{
-			bytes_transferred = co_await impl.socket->async_read_some(
-				buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			if constexpr (ssl_mode<SSL>)
+			{
+				bytes_transferred = co_await impl.ssl_socket->async_read_some(
+					buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			}
+			else
+			{
+				bytes_transferred = co_await impl.socket->async_read_some(
+					buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			}
 		}
 
 		template <typename ConstBuffer>
 		auto async_write_some(implementation_type& impl, const ConstBuffer& buffer, boost::system::error_code& ec)
 			-> boost::asio::awaitable<void>
 		{
-			co_await impl.socket->async_write_some(buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			if constexpr (ssl_mode<SSL>)
+			{
+				co_await impl.ssl_socket->async_write_some(buffer,
+														   boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			}
+			else
+			{
+				co_await impl.socket->async_write_some(buffer,
+													   boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			}
 		}
 
-		void start(implementation_type& impl)
+		auto start(implementation_type& impl) -> boost::asio::awaitable<void>
 		{
-			XLOG_INFO() << "handshake success at " << io_base::remote_address(impl) << ":" << io_base::remote_port(impl)
+			XLOG_INFO() << "handshake success at " << remote_address(impl) << ":" << remote_port(impl)
 						<< ", async read establish";
+
+			if constexpr (ssl_mode<SSL>)
+			{
+				boost::system::error_code ec;
+
+				co_await impl.ssl_socket->async_handshake(handshake,
+														  boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			}
 
 			this->keep_alive(impl, true);
 
 			this->set_nodelay(impl, true);
-		}
-	};
-
-#ifdef AQUARIUS_ENABLE_SSL
-	struct ssl
-	{};
-
-	template <typename Protocol, typename Executor>
-	class async_io_service<Protocol, Executor, std::void_t<ssl>>
-		: public boost::asio::detail::execution_context_service_base<async_io_service<Protocol, Executor, SSL>>,
-		  public async_io_base<Protocol, Executor>
-	{
-	public:
-		using socket_type = boost::asio::basic_stream_socket<Protocol, Executor>;
-
-		using io_base = async_io_base<Protocol, Executor>;
-
-		using resolver_type = typename Protocol::resolver;
-
-		using ssl_socket_type = boost::asio::ssl::stream<socket_type&>;
-
-		using ssl_context_type = boost::asio::ssl::context;
-
-		using executor_type = Executor;
-
-		using ssl_type = ssl;
-
-		using base_type =
-			boost::asio::detail::execution_context_service_base<async_io_service<Protocol, Executor, SSL>>;
-
-		struct implementation_type : io_base::implementation_type_base
-		{
-			ssl_socket_type* ssl_socket;
-		};
-
-	public:
-		explicit async_io_service(const Executor& ex)
-			: base_type(ex)
-		{}
-
-		async_io_service(boost::asio::execution_context& context)
-			: base_type(context)
-		{}
-
-	public:
-		void construct(implementation_type& impl)
-		{}
-
-		void move_copy(implementation_type& impl, socket_type socket, ssl_context_type& ssl_context)
-		{
-			this->base_move_copy(impl, socket);
-
-			impl.ssl_socket = new ssl_socket_type(*impl.socket, ssl_context);
-
-			//if constexpr (!IsServer)
-			//{
-			//	impl.ssl_socket.set_verify_mode(boost::asio::ssl::verify_peer);
-			//}
-		}
-
-		void destroy(implementation_type& impl)
-		{
-			this->base_destroy(impl);
-
-			if (impl.ssl_socket)
-			{
-				boost::system::error_code ec;
-
-				impl.ssl_socket->shutdown(ec);
-
-				delete impl.ssl_socket;
-			}
-		}
-
-		virtual void shutdown() override
-		{
-			return;
-		}
-
-		auto async_connect(implementation_type& impl, const std::string& ip_addr, const std::string& port,
-			boost::system::error_code& ec) -> boost::asio::awaitable<void>
-		{
-			resolver_type resolver(impl.socket->get_executor());
-
-			auto endpoint = resolver.resolve(ip_addr, port);
-
-			co_await impl.socket->async_connect(endpoint, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-		}
-
-		template <typename ConstBuffer>
-		auto async_read_some(implementation_type& impl, ConstBuffer& buffer, std::size_t& bytes_transferred,
-			boost::system::error_code& ec) -> boost::asio::awaitable<void>
-		{
-			bytes_transferred = co_await impl.ssl_socket->async_read_some(
-				buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-		}
-
-		template <typename ConstBuffer>
-		auto async_write_some(implementation_type& impl, const ConstBuffer& buffer, boost::system::error_code& ec)
-			-> boost::asio::awaitable<void>
-		{
-			co_await impl.ssl_socket->async_write_some(buffer, boost::asio::redirect_error(boost::asio::use_awaitable, ec));
-		}
-
-		void start(implementation_type& impl)
-		{
-			XLOG_INFO() << "handshake success at " << io_base::remote_address(impl) << ":" << io_base::remote_port(impl)
-						<< ", async read establish";
-
-			boost::system::error_code ec{};
-			handshake(impl, ec);
-
-			if (!ec)
-			{
-				return;
-			}
-
-			keep_alive(impl, true);
-
-			set_nodelay(impl, true);
-		}
-
-		void close(implementation_type& impl, bool enable)
-		{
-			boost::system::error_code ec;
-
-			if (!impl.socket->is_open())
-				return;
-
-			if (enable)
-				return this->base_shutdown(impl);
-
-			impl.socket->close(ec);
 		}
 
 		bool keep_alive(implementation_type& impl, bool value)
@@ -257,13 +188,19 @@ namespace aquarius
 			return !ec;
 		}
 
-	private:
-		auto handshake(implementation_type& impl, bool is_server, boost::system::error_code& ec) -> boost::asio::awaitable<void>
+		std::string remote_address(const implementation_type& impl) const
 		{
-			co_await impl.ssl_socket->async_handshake(
-				static_cast<boost::asio::ssl::stream_base::handshake_type>(is_server),
-				boost::asio::redirect_error(boost::asio::use_awaitable, ec));
+			return impl.socket->remote_endpoint().address().to_string();
+		}
+
+		uint32_t remote_address_u(const implementation_type& impl) const
+		{
+			return impl.socket->remote_endpoint().address().to_v4().to_uint();
+		}
+
+		uint16_t remote_port(const implementation_type& impl) const
+		{
+			return impl.socket->remote_endpoint().port();
 		}
 	};
-#endif
 } // namespace aquarius
---
+++ include/aquarius/detail/ssl_traits.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <concepts>
+#include <boost/system/error_code.hpp>
+
+namespace aquarius
+{
+#ifdef AQUARIUS_ENABLE_SSL
+	struct ssl
+	{
+		static constexpr auto version = boost::asio::ssl::context_base::sslv23;
+	};
+#else
+	struct ssl
+	{
+		static constexpr std::size_t version = -1;
+	};
+#endif
+
+	template <typename T>
+	concept ssl_mode = !std::is_same_v<std::decay_t<T>, void>;
+
+	template <typename Socket, typename SSLContext>
+	class null_ssl
+	{
+	public:
+		explicit null_ssl(Socket&, SSLContext) {};
+
+		void shutdown(boost::system::error_code)
+		{}
+
+		template <typename... Args>
+		auto async_handshake(Args&&... args) -> boost::asio::awaitable<void>
+		{
+			co_return;
+		}
+	};
+
+	template <typename T>
+	struct ssl_version_traits
+	{
+		static constexpr std::size_t value = -1;
+	};
+
+	template <>
+	struct ssl_version_traits<ssl>
+	{
+		static constexpr auto value = ssl::version;
+	};
+} // namespace aquarius
