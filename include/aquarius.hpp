d2a55b15e418d094f1eb6207a62500f3d2567491
kcwl <767899122@qq.com>
2025-05-11 14:53:53 +0800

    fix crc32

--- include/aquarius.hpp
+++ include/aquarius.hpp
@@ -3,7 +3,7 @@
 #include <aquarius/client.hpp>
 #include <aquarius/server.hpp>
 #include <aquarius/core/ini_tree.hpp>
-#include <aquarius/core/crc.hpp>
+#include <aquarius/crc.hpp>
 #include <aquarius/core/singleton.hpp>
 #include <aquarius/session.hpp>
 #include <aquarius/detail/context.hpp>
