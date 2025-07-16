cb853eb257bdc189d54970519b1159d07346b99c
kcwl <767899122@qq.com>
2025-07-16 22:55:27 +0800

    fix for serviced-ssh

--- src/serviced-ssh/serviced-ssh.cpp
+++ src/serviced-ssh/serviced-ssh.cpp
@@ -46,7 +46,7 @@ int main(int argc, char* argv[])
         if (input == "quit" || input == "q")
             break;
 
-        CLIENT.async_send()
+        //CLIENT.async_send()
     }
 
     return 0;
