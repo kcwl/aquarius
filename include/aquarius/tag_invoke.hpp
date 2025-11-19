ab1273b4bf448db732a9bca87a242c272fafbaf4
kcwl <767899122@qq.com>
2025-11-19 20:05:24 +0800

    fix for tag invoke

--- include/aquarius/tag_invoke.hpp
+++ include/aquarius/tag_invoke.hpp
@@ -1,17 +1,33 @@
 #pragma once
+#include <concepts>
 
 namespace aquarius
 {
-	struct http_param_tag;
-	struct http_param;
-}
-
-namespace aquarius
-{
-	template <typename T>
-	struct value_to
-	{};
+	template<typename T>
+	struct value_to_tag {};
 
 
-	void tag_invoke(value_to<http_param_tag>, http_param&);
+	void tag_invoke(value_to_tag<std::nullopt_t>, std::nullopt_t)
+	{
+		return;
+	}
+
+
+	template<typename T, typename P, typename = void>
+	struct has_tag_invokable : std::false_type {};
+
+	template<typename T, typename P>
+	struct has_tag_invokable<T, P, decltype(tag_invoke(value_to_tag<T>(), std::declval<P&>()))> : std::true_type {};
+
+	template<typename T, typename P>
+	void value_to(P& t)
+	{
+		constexpr bool b = has_tag_invokable<T, P>::value;
+
+		if constexpr (b)
+		{
+			tag_invoke(value_to_tag<T>(), t);
+		}
+	}
+
 } // namespace aquarius
