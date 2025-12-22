658211ec3d07d2f98c414ab3f2cf6d64248058e3
kcwl <767899122@qq.com>
2025-12-22 22:28:57 +0800

    fix for basic_datatype that not used

--- include/aquarius/tbl.hpp
+++ include/aquarius/tbl.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <aquarius/tbl/basic_datatype.hpp>
+//#include <aquarius/tbl/basic_datatype.hpp>
 #include <aquarius/tbl/engine/bool.hpp>
 #include <aquarius/tbl/engine/field.hpp>
 #include <aquarius/tbl/engine/float.hpp>
@@ -15,31 +15,31 @@ namespace aquarius
 {
 	namespace tbl
 	{
-		using int1 = basic_datatype<uint8_t, integer>;
-		using int2 = basic_datatype<uint16_t, integer>;
-		using int3 = basic_datatype<int32_t, integer>;
-		using int4 = basic_datatype<uint32_t, integer>;
-		using int8 = basic_datatype<uint64_t, integer>;
+		//using int1 = basic_datatype<uint8_t, integer>;
+		//using int2 = basic_datatype<uint16_t, integer>;
+		//using int3 = basic_datatype<int32_t, integer>;
+		//using int4 = basic_datatype<uint32_t, integer>;
+		//using int8 = basic_datatype<uint64_t, integer>;
 
-		using tinytext = basic_text<uint8_t, vtext>;
-		using text = basic_text<uint16_t, vtext>;
-		using mediumtext = basic_text<uint32_t, vtext>;
-		using longtext = basic_text<uint64_t, vtext>;
+		//using tinytext = basic_text<uint8_t, vtext>;
+		//using text = basic_text<uint16_t, vtext>;
+		//using mediumtext = basic_text<uint32_t, vtext>;
+		//using longtext = basic_text<uint64_t, vtext>;
 
-		using tinyblob = basic_text<uint8_t, blob_impl>;
-		using blob = basic_text<uint16_t, blob_impl>;
-		using mediumblob = basic_text<uint32_t, blob_impl>;
-		using longblob = basic_text<uint64_t, blob_impl>;
+		//using tinyblob = basic_text<uint8_t, blob_impl>;
+		//using blob = basic_text<uint16_t, blob_impl>;
+		//using mediumblob = basic_text<uint32_t, blob_impl>;
+		//using longblob = basic_text<uint64_t, blob_impl>;
 
-		using boolean = basic_datatype<bool, _bool>;
-		using floatable = basic_datatype<float, _float>;
-		using doublable = basic_datatype<double, _double>;
+		//using boolean = basic_datatype<bool, _bool>;
+		//using floatable = basic_datatype<float, _float>;
+		//using doublable = basic_datatype<double, _double>;
 
-		template <auto N>
-		using dchar = basic_char<N>;
+		//template <auto N>
+		//using dchar = basic_char<N>;
 
-		using varchar = basic_datatype<std::string, _varchar>;
+		//using varchar = basic_datatype<std::string, _varchar>;
 
-		using nvarchar = basic_datatype<std::wstring, _nvarchar>;
+		//using nvarchar = basic_datatype<std::wstring, _nvarchar>;
 	} // namespace tbl
 } // namespace aquarius
