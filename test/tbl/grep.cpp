ad897336edb40b92dba886ee313f6e063503a0f2
kcwl <767899122@qq.com>
2026-04-18 07:44:07 +0800

    fix for grep

--- test/tbl/grep.cpp
+++ test/tbl/grep.cpp
@@ -14,44 +14,44 @@ BOOST_AUTO_TEST_SUITE(ut_grep_view)
 
 BOOST_AUTO_TEST_CASE(less)
 {
-	auto& result = grep<&mock_tbl::a>() < 3;
+	auto result = std::move(grep<&mock_tbl::a>() < 3);
 
 	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a < 3");
 }
 
 BOOST_AUTO_TEST_CASE(less_euqal)
 {
-	auto& result = grep<&mock_tbl::a>() <= 3;
+	auto result = std::move(grep<&mock_tbl::a>() <= 3);
 
 	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a <= 3");
 }
 
 BOOST_AUTO_TEST_CASE(great)
 {
-	auto& result = grep<&mock_tbl::a>() > 3;
+	auto result = std::move(grep<&mock_tbl::a>() > 3);
 
 	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a > 3");
 }
 
 BOOST_AUTO_TEST_CASE(great_equal)
 {
-	auto& result = grep<&mock_tbl::a>() > 3;
+	auto result = std::move(grep<&mock_tbl::a>() >= 3);
 
 	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a >= 3");
 }
 
 BOOST_AUTO_TEST_CASE(equal)
 {
-	auto& result = grep<&mock_tbl::a>() == 3;
+	auto result = std::move(grep<&mock_tbl::a>() == 3);
 
 	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a = 3");
 }
 
 BOOST_AUTO_TEST_CASE(_or_)
 {
-	auto& result = grep<&mock_tbl::a>() >= 3 || grep<&mock_tbl::a>() <= 1;
+	auto result = std::move(grep<&mock_tbl::a>() >= 3 || grep<&mock_tbl::a>() <= 1);
 
-	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "a >= 3 or a <= 1");
+	BOOST_CHECK_EQUAL(static_cast<std::string>(result), "(a >= 3 or a <= 1)");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
