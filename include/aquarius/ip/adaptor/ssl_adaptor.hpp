a05f17288173e33974a0a72106b400b64c54f7d7
kcwl <767899122@qq.com>
2026-04-17 19:36:08 +0800

    fix for ssl adaptor unittest

--- include/aquarius/ip/adaptor/ssl_adaptor.hpp
+++ include/aquarius/ip/adaptor/ssl_adaptor.hpp
@@ -6,113 +6,117 @@
 
 namespace aquarius
 {
-    template<auto SSLVersion, bool Server>
-    class basic_ssl_context
-    {
-    public:
-        asio::ssl::context create()
-        {
-            asio::ssl::context ssl_context(SSLVersion);
+	template <auto SSLVersion, bool Server>
+	class basic_ssl_context
+	{
+	public:
+		asio::ssl::context& create()
+		{
+			static asio::ssl::context ssl_context(SSLVersion);
 
-            if constexpr (Server)
-            {
-                ssl_context.set_options(asio::ssl::context::default_workarounds | asio::ssl::context::no_sslv2 |
-                                        asio::ssl::context::single_dh_use);
+			if constexpr (Server)
+			{
+				ssl_context.set_options(asio::ssl::context::default_workarounds | asio::ssl::context::no_sslv2 |
+										asio::ssl::context::single_dh_use);
 
-                ssl_context.use_certificate_chain_file("crt/server.crt");
-                ssl_context.use_private_key_file("crt/server.key", asio::ssl::context::pem);
-                ssl_context.use_tmp_dh_file("crt/dh2048.pem");
+				ssl_context.use_certificate_chain_file("crt/server.crt");
+				ssl_context.use_private_key_file("crt/server.key", asio::ssl::context::pem);
+				ssl_context.use_tmp_dh_file("crt/dh2048.pem");
+			}
+			else
+			{
+				auto path = std::filesystem::current_path();
 
-                return ssl_context;
-            }
-            else
-            {
-                auto path = std::filesystem::current_path();
+				path.append("crt");
+				path.append("server.crt");
 
-                path.append("crt");
-                path.append("server.crt");
+				ssl_context.load_verify_file(path.string());
+			}
 
-                ssl_context.load_verify_file(path.string());
-            }
-        }
-    };
+			return ssl_context;
+		}
+	};
 
-    template<bool Server, typename T, auto SSLVersion = asio::ssl::context::tlsv13>
-    class ssl_adaptor : public basic_ssl_context<SSLVersion, Server>
-    {
-    public:
-        using socket = T;
+	template <bool Server, typename T, auto SSLVersion = asio::ssl::context::tlsv13>
+	class ssl_adaptor : public basic_ssl_context<SSLVersion, Server>
+	{
+	public:
+		using socket = T;
 
-        using ssl_socket = asio::ssl::stream<socket&>;
+		using ssl_socket = asio::ssl::stream<socket&>;
 
-    public:
-        ssl_adaptor(socket& socket)
-            : socket_(socket, this->create())
-        {
-            init();
-        }
+		using base = basic_ssl_context<SSLVersion, Server>;
 
-    public:
-        ssl_socket& get_implement()
-        {
-            return socket_;
-        }
+	public:
+		ssl_adaptor(socket& socket)
+			: socket_(socket, this->create())
+		{
+			init();
+		}
 
-        template <typename Endpoint, typename Dura>
-        auto async_connect(const Endpoint& endpoint, Dura timeout) -> asio::awaitable<error_code>
-        {
-            error_code ec;
+	public:
+		ssl_socket& get_implement()
+		{
+			return socket_;
+		}
 
-            co_await async_connect(socket_.lowest_layer(), endpoint,
-                                   asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
+		template <typename Endpoint, typename Dura>
+		auto async_connect(const Endpoint& endpoint, Dura timeout) -> asio::awaitable<error_code>
+		{
+			error_code ec;
 
-            if (!ec)
-            {
-                co_await socket_.async_handshake(asio::ssl::stream_base::client,
-                                                 asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
-            }
+			co_await asio::async_connect(socket_.lowest_layer(), endpoint,
+										 asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
 
-            co_return ec;
-        }
+			if (!ec)
+			{
+				co_await socket_.async_handshake(
+					asio::ssl::stream_base::client,
+					asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
+			}
 
-        template <typename Dura>
-        auto async_handshake(Dura timeout) -> asio::awaitable<error_code>
-        {
-            error_code ec;
+			co_return ec;
+		}
 
-            co_await socket_.async_handshake(asio::ssl::stream_base::server,
-                                             asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
+		template <typename Dura>
+		auto async_handshake(Dura timeout) -> asio::awaitable<error_code>
+		{
+			error_code ec;
 
-            co_return ec;
-        }
+			co_await socket_.async_handshake(
+				asio::ssl::stream_base::server,
+				asio::cancel_after(timeout, asio::redirect_error(asio::use_awaitable, ec)));
 
-    private:
-        void init()
-        {
-            if constexpr (!Server)
-            {
-                socket_.set_verify_mode(asio::ssl::verify_peer);
+			co_return ec;
+		}
 
-                socket_.set_verify_callback(
-                    [] (bool verify, asio::ssl::verify_context& ctx)
-                    {
-                        char subject_name[256];
-                        X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
-                        X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
-                        XLOG_INFO() << "Verifying " << subject_name << "\n";
+	private:
+		void init()
+		{
+			if constexpr (!Server)
+			{
+				socket_.set_verify_mode(asio::ssl::verify_peer);
 
-                        return verify;
-                    });
-            }
-        }
+				socket_.set_verify_callback(
+					[](bool verify, asio::ssl::verify_context& ctx)
+					{
+						char subject_name[256];
+						X509* cert = X509_STORE_CTX_get_current_cert(ctx.native_handle());
+						X509_NAME_oneline(X509_get_subject_name(cert), subject_name, 256);
+						XLOG_INFO() << "Verifying " << subject_name << "\n";
 
-    private:
-        ssl_socket socket_;
-    };
+						return verify;
+					});
+			}
+		}
 
-    template<typename T>
-    using ssl_client_adaptor = ssl_adaptor<false, T>;
+	private:
+		ssl_socket socket_;
+	};
 
-    template<typename T>
-    using ssl_server_adaptor = ssl_adaptor<true, T>;
+	template <typename T>
+	using ssl_client_adaptor = ssl_adaptor<false, T>;
+
+	template <typename T>
+	using ssl_server_adaptor = ssl_adaptor<true, T>;
 } // namespace aquarius
--- test/ip/ssl_adaptor_test.cpp
+++ test/ip/ssl_adaptor_test.cpp
@@ -1,45 +1,151 @@
 #define BOOST_TEST_NO_MAIN
-#include <boost/test/included/unit_test.hpp>
-#include <boost/asio.hpp>
-#include <boost/asio/ssl.hpp>
-
-#include "../include/aquarius/ip/adaptor/ssl_adaptor.hpp"
+#include <boost/test/unit_test.hpp>
+#include <aquarius.hpp>
 
 using namespace aquarius;
 
-BOOST_AUTO_TEST_CASE(construct_client_and_server_contexts)
+template<typename Protocol, typename Executor = asio::any_io_executor>
+struct mock_socket : public asio::basic_socket<Protocol, Executor>
 {
-    // Create a dummy io_context and a socket to construct ssl_adaptor
-    boost::asio::io_context ioc;
-    boost::asio::ip::tcp::socket sock(ioc);
+	mock_socket(asio::io_context& io)
+		: asio::basic_socket<Protocol, Executor>(io)
+	{}
+	struct initiate_async_connect
+	{
+		initiate_async_connect(initiate_async_connect*) {}
+	};
 
-    // Construct server adaptor - should not throw even if cert files missing
-    BOOST_CHECK_NO_THROW({
-        ssl_adaptor<true> server_adaptor(sock);
-        auto& impl = server_adaptor.get_implement();
-        (void)impl;
-    });
+	template<typename Token, typename Endpoint>
+	auto async_connect(const Token& peer_endpoint,
+					   Token&& token)
+		-> decltype(
+			asio::async_initiate<Token, void(error_code)>(
+				std::declval<initiate_async_connect>(), token,
+				peer_endpoint, std::declval<error_code&>()))
+	{
+		boost::system::error_code open_ec;
+		return async_initiate<Token, void(boost::system::error_code)>(
+			initiate_async_connect(this), token, peer_endpoint, open_ec);
+	}
 
-    // Construct client adaptor - should not throw
-    BOOST_CHECK_NO_THROW({
-        ssl_adaptor<false> client_adaptor(sock);
-        auto& impl = client_adaptor.get_implement();
-        (void)impl;
-    });
+	template<typename Buffer, typename Token>
+	auto async_read_some(Buffer, Token) -> asio::awaitable<error_code>
+	{
+		co_return error_code{};
+	}
+
+	template<typename Buffer, typename Token>
+	auto async_write_some(Buffer, Token) -> asio::awaitable<error_code>
+	{
+		co_return error_code{};
+	}
+};
+
+BOOST_AUTO_TEST_SUITE(client_ssls)
+
+BOOST_AUTO_TEST_CASE(ctor)
+{
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_client_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
+
+	auto result = socket.get_executor() == adaptor.get_implement().get_executor();
+	BOOST_TEST(result);
 }
 
-BOOST_AUTO_TEST_CASE(verify_callback_is_callable)
+BOOST_AUTO_TEST_CASE(connect)
 {
-    boost::asio::io_context ioc;
-    boost::asio::ip::tcp::socket sock(ioc);
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_client_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
 
-    ssl_adaptor<false> client_adaptor(sock);
-    auto& impl = client_adaptor.get_implement();
+	auto future = asio::co_spawn(
+		io,
+		[&] () -> asio::awaitable<void>
+		{
+			asio::ip::tcp::resolver resolve{ io };
 
-    // The verify callback should be set for client adaptor. We can't easily invoke OpenSSL's verify flow
-    // without a real handshake, but we can ensure the callback can be called through the SSL object.
+			auto ec = co_await adaptor.async_connect(resolve.resolve({}, {}), 1s);
+			BOOST_TEST(!ec);
+		},
+		asio::use_future);
 
-    // Prepare a verify_context manually using OpenSSL structures
-    // This is a light-weight test to ensure the code compiles and the callback is assignable.
-    BOOST_CHECK(true);
+	io.run();
+
+	future.get();
 }
+
+BOOST_AUTO_TEST_CASE(handshake)
+{
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_client_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
+
+	auto future = asio::co_spawn(
+		io,
+		[&] () -> asio::awaitable<void>
+		{
+			asio::ip::tcp::resolver resolve{ io };
+
+			auto ec = co_await adaptor.async_handshake(1s);
+			BOOST_TEST(!ec);
+		},
+		asio::use_future);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+BOOST_AUTO_TEST_SUITE(server_ssls)
+
+BOOST_AUTO_TEST_CASE(ctor)
+{
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_server_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
+
+	auto result = socket.get_executor() == adaptor.get_implement().get_executor();
+	BOOST_TEST(result);
+}
+
+BOOST_AUTO_TEST_CASE(connect)
+{
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_server_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
+
+	auto future = asio::co_spawn(
+		io,
+		[&] () -> asio::awaitable<void>
+		{
+			asio::ip::tcp::resolver resolve{ io };
+
+			auto ec = co_await adaptor.async_connect(resolve.resolve({}, {}), 1s);
+			BOOST_TEST(!ec);
+		},
+		asio::use_future);
+
+	io.run();
+
+	future.get();
+}
+
+BOOST_AUTO_TEST_CASE(handshake)
+{
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	ssl_server_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
+
+	auto future = asio::co_spawn(
+		io,
+		[&] () -> asio::awaitable<void>
+		{
+			asio::ip::tcp::resolver resolve{ io };
+
+			auto ec = co_await adaptor.async_handshake(1s);
+			BOOST_TEST(!ec);
+		},
+		asio::use_future);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
