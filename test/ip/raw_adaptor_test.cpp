682042ba4c4d17b4f3dbcd0b354070a1ed3806fd
kcwl <767899122@qq.com>
2026-04-17 08:11:36 +0800

    fix for adaptor with mock socket

--- test/ip/raw_adaptor_test.cpp
+++ test/ip/raw_adaptor_test.cpp
@@ -7,37 +7,49 @@
 using namespace aquarius;
 using namespace std::chrono_literals;
 
-namespace boost
-{
-	namespace asio
-	{
-		template<typename Socket, typename Endpoint, typename Token>
-		auto async_connect(Socket&, Endpoint, Token) -> asio::awaitable<error_code>
-		{
-			co_return error_code{};
-		}
-	}
-}
 
-struct mock_socket
-{};
+template<typename Protocol, typename Executor = asio::any_io_executor>
+struct mock_socket : public asio::basic_socket<Protocol, Executor>
+{
+	mock_socket(asio::io_context& io)
+		: asio::basic_socket<Protocol, Executor>(io)
+	{}
+	struct initiate_async_connect 
+	{
+		initiate_async_connect(initiate_async_connect*) {}
+	};
+
+		template<typename Token, typename Endpoint>
+		auto async_connect(const Token& peer_endpoint,
+						   Token&& token)
+		-> decltype(
+			asio::async_initiate<Token, void(error_code)>(
+				std::declval<initiate_async_connect>(), token,
+				peer_endpoint, std::declval<error_code&>()))
+	{
+			boost::system::error_code open_ec;
+		return async_initiate<Token, void(boost::system::error_code)>(
+			initiate_async_connect(this), token, peer_endpoint, open_ec);
+	}
+};
 
 BOOST_AUTO_TEST_SUITE(ut_raw_adaptor)
 
 BOOST_AUTO_TEST_CASE(ctor)
 {
-	mock_socket socket;
-	raw_adaptor<mock_socket> adaptor(socket);
+	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	raw_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
 
-	BOOST_CHECK_EQUAL(socket, adaptor.get_implement());
+	auto result = socket.get_executor() == adaptor.get_implement().get_executor();
+	BOOST_TEST(result);
 }
 
 BOOST_AUTO_TEST_CASE(connect)
 {
-	mock_socket socket;
-	raw_adaptor<mock_socket> adaptor(socket);
-
 	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	raw_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
 
 	auto future = asio::co_spawn(
 		io,
@@ -49,14 +61,17 @@ BOOST_AUTO_TEST_CASE(connect)
 			BOOST_TEST(!ec);
 		},
 		asio::use_future);
+
+	io.run();
+
+	future.get();
 }
 
 BOOST_AUTO_TEST_CASE(handshake)
 {
-	mock_socket socket;
-	raw_adaptor<mock_socket> adaptor(socket);
-
 	asio::io_context io{};
+	mock_socket<asio::ip::tcp> socket(io);
+	raw_adaptor<mock_socket<asio::ip::tcp>> adaptor(socket);
 
 	auto future = asio::co_spawn(
 		io,
