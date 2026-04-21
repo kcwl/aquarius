5b81f439aaf8b437434d845715e28409c6a9d440
kcwl <767899122@qq.com>
2026-04-22 07:15:25 +0800

    update auth module

--- src/gateway/auth_module.cpp
+++ src/gateway/auth_module.cpp
@@ -1,26 +1,13 @@
 #include "auth_module.h"
-#include "table/account.tbl.h"
 #include <aquarius/module/module_router.hpp>
-#include <aquarius/module/sql_module.hpp>
 
 namespace aquarius
 {
 	namespace gateway
 	{
-		auth_module::auth_module(io_context& io, const std::string& name)
-			: no_config_module<auth_module>(io, name)
-		{}
-
-		auto auth_module::auth(const std::string& username, const std::string& password) -> awaitable<bool>
+		auto auth_module::auth(const std::string& username, const std::string& password) -> asio::awaitable<bool>
 		{
-			std::string sql =
-				std::format("select * from account where account={} and passwd={}", username, password);
-
-			auto accounts = co_await mpc_query<account>(sql);
-
-			co_return !accounts.empty();
+			co_return true;
 		}
 	} // namespace gateway
 } // namespace aquarius
-
-AQUARIUS_MODULE_NAMESPACE(aquarius::gateway, auth_module)
