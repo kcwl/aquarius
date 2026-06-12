913a161a47d7be28d1d9bc1d957e3f4ac7cf312b
kcwl <767899122@qq.com>
2026-06-12 08:09:13 +0800

    update ut_io_service_pool of 94%

--- test/ut_io_service_pool.cpp
+++ test/ut_io_service_pool.cpp
@@ -1,17 +1,33 @@
-#include <boost/test/unit_test.hpp>
 #include <aquarius/io_service_pool.hpp>
+#include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(ut_io_service_pool)
 
-BOOST_AUTO_TEST_CASE(construction_and_access)
+using namespace aquarius;
+using namespace std::chrono_literals;
+
+BOOST_AUTO_TEST_CASE(ctor)
 {
-    aquarius::io_service_pool pool(1);
-    BOOST_TEST(pool.size() == 1);
+	io_service_pool io(10);
 
-    auto& io = pool.get_io_service();
-    (void)io;
+	BOOST_TEST(io.capacity() == 10);
 
-    BOOST_TEST(pool.pool_size() == 1);
+	BOOST_REQUIRE_THROW(io_service_pool zero(0), std::runtime_error);
 }
 
+BOOST_AUTO_TEST_CASE(run)
+{
+	io_service_pool io(10);
+
+	std::thread t([&] { io.run(); });
+
+	std::this_thread::sleep_for(2s);
+
+	BOOST_TEST(io.enable());
+
+	io.stop();
+
+	BOOST_TEST(!io.enable());
+	t.join();
+}
 BOOST_AUTO_TEST_SUITE_END()
