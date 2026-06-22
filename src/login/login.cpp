28e32cfcc9f4c26b62953f9d13802c6cd9eb6dc7
kcwl <767899122@qq.com>
2026-06-22 22:07:27 +0800

    fix for login on http

--- src/login/login.cpp
+++ src/login/login.cpp
@@ -5,15 +5,108 @@
 #include <aquarius.hpp>
 #include <srvd_client.hpp>
 
-using namespace aquarius;
+namespace po = boost::program_options;
 
-int main()
+int main(int argc, char* argv[])
 {
-    srv_config::get_mutable_instance().host = "127.0.0.1";
-    srv_config::get_mutable_instance().port = 3399;
-    tcp::server srv(3344, 10, "login");
+    po::options_description cmd("login");
 
-    srv.run();
+    std::string name("gateway");
+    int32_t pool_size = static_cast<int32_t>(std::thread::hardware_concurrency());
+
+    cmd.add_options()("help", "print help message");
+    cmd.add_options()("listen", po::value<uint16_t>(), "listen port");
+    cmd.add_options()("pool_size", po::value<int32_t>(&pool_size), "pool thread size");
+    cmd.add_options()("name", po::value<std::string>(&name), "server name");
+    cmd.add_options()("proto", po::value<std::string>(), "proto that tcp and http1.1");
+    cmd.add_options()("srvd_host", po::value<std::string>(), "serviced host");
+    cmd.add_options()("srvd_port", po::value<uint16_t>(), "serviced port");
+
+    uint16_t port{};
+
+    po::variables_map vm{};
+    po::store(po::parse_command_line(argc, argv, cmd), vm);
+    po::notify(vm);
+
+    std::string proto{};
+
+
+    if (vm.count("help"))
+    {
+        std::cout << cmd;
+        return 0;
+    }
+
+    if (vm.count("listen"))
+    {
+        port = vm["listen"].as<uint16_t>();
+    }
+    else
+    {
+        std::cout << "the listen port is invalid\n";
+        std::cout << cmd;
+        return 0;
+    }
+
+    if (vm.count("pool_size"))
+    {
+        pool_size = vm["pool_size"].as<int32_t>();
+    }
+
+    if (vm.count("name"))
+    {
+        name = vm["name"].as<std::string>();
+    }
+
+    if (vm.count("proto"))
+    {
+        proto = vm["proto"].as<std::string>();
+    }
+    else
+    {
+        std::cout << "the proto is invalid\n";
+        std::cout << cmd;
+        return 0;
+    }
+
+    if (vm.count("srvd_host"))
+    {
+        srv_config::get_mutable_instance().host = vm["srvd_host"].as<std::string>();
+    }
+    else
+    {
+        std::cout << "the srvd_host is invalid\n";
+        std::cout << cmd;
+        return 0;
+    }
+
+    if (vm.count("srvd_port"))
+    {
+        srv_config::get_mutable_instance().port = vm["srvd_port"].as<uint16_t>();
+    }
+    else
+    {
+        std::cout << "the srvd_port is invalid\n";
+        std::cout << cmd;
+        return 0;
+    }
+
+    if (proto == "tcp")
+    {
+        aquarius::tcp::server server(port, pool_size, name);
+
+        server.run();
+    }
+    else if (proto == "http1.1")
+    {
+        aquarius::http::server server(port, pool_size, name);
+
+        server.run();
+    }
+    else
+    {
+        std::cout << "the proto [" << proto << "] not support!\n";
+    }
 
     return 0;
 }
--- src/login/proto/login.virgo.h
+++ src/login/proto/login.virgo.h
@@ -1,10 +1,10 @@
 #pragma once
-#include <aquarius/serialize/tcp_serialize.hpp>
-#include <aquarius/virgo/tcp_request.hpp>
-#include <aquarius/virgo/tcp_response.hpp>
+#include <aquarius/serialize/http_serialize.hpp>
+#include <aquarius/virgo/http_request.hpp>
+#include <aquarius/virgo/http_response.hpp>
 using namespace aquarius;
 
-class login_req_body: public aquarius::tcp_serialize
+class login_req_body: public aquarius::http_serialize
 {
 public:
 	login_req_body();
@@ -12,6 +12,9 @@ public:
 
 	login_req_body(login_req_body&&) = default;
 	login_req_body& operator=(login_req_body&&) = default;
+private:
+	friend void tag_invoke(const aquarius::json::value_from_tag&, aquarius::json::value& jv, const login_req_body& local);
+	friend login_req_body tag_invoke(const aquarius::json::value_to_tag<login_req_body>&, const aquarius::json::value& jv);
 
 public:
 	virtual void serialize(aquarius::flex_buffer& buffer) override;
@@ -29,7 +32,7 @@ private:
 	struct impl;
 	std::shared_ptr<impl> impl_ptr_;
 };
-class login_resp_body: public aquarius::tcp_serialize
+class login_resp_body: public aquarius::http_serialize
 {
 public:
 	login_resp_body();
@@ -37,6 +40,9 @@ public:
 
 	login_resp_body(login_resp_body&&) = default;
 	login_resp_body& operator=(login_resp_body&&) = default;
+private:
+	friend void tag_invoke(const aquarius::json::value_from_tag&, aquarius::json::value& jv, const login_resp_body& local);
+	friend login_resp_body tag_invoke(const aquarius::json::value_to_tag<login_resp_body>&, const aquarius::json::value& jv);
 
 public:
 	virtual void serialize(aquarius::flex_buffer& buffer) override;
@@ -50,5 +56,5 @@ private:
 };
 
 
-using login_request = aquarius::tcp_request<"10000", login_req_body>;
-using login_response = aquarius::tcp_response<"10000", login_resp_body>;
+using login_request = aquarius::http_request<"10000", login_req_body>;
+using login_response = aquarius::http_response<login_resp_body>;
